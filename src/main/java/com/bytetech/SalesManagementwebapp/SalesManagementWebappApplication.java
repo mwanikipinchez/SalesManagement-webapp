065c6e0dea7051089954fba7218ea5e61de01458
Peter Mwaniki <mwanikipinchez@gmail.com>
2023-03-17 23:58:03 +0300

    Initial commit

---
+++ src/main/java/com/bytetech/SalesManagementwebapp/SalesManagementWebappApplication.java
@@ -0,0 +1,13 @@
+package com.bytetech.SalesManagementwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesManagementWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesManagementWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytetech/SalesManagementwebapp/SalesManagementWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytetech.SalesManagementwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesManagementWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
